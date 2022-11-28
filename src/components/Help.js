6c95cf70e41adeff89d07c4e828cc538e35e5d77
prod <prod@gcloud.10z.dev>
2022-11-28 05:43:06 +0000

    add documents

--- src/components/Help.js
+++ src/components/Help.js
@@ -7,6 +7,32 @@ export default function Help({ liff }) {
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
+      <div
+  style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}
+>
+  <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+
+  <div>
+    <p style={{width: '70px', textAlign: 'center'}}>Download</p>
+  </div>
+
+  <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+</div>
+       <ul>
+        <li><a href={`${process.env.PUBLIC_URL}/01_Manual.pdf`}>Download Manual</a></li>
+        <li><a href={`${process.env.PUBLIC_URL}/02_Pick_up_points.pdf`}>Download Pick-up points</a></li>
+        </ul>
+        <div
+  style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}
+>
+  <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+
+  <div>
+    <p style={{width: '70px', textAlign: 'center'}}>Application Information</p>
+  </div>
+
+  <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+</div>
       <ul>
         <li>LIFF app version: {version}</li>
         <li>Language: {liff.getLanguage()}</li>
